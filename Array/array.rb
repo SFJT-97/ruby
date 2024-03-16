6a0b022b1d9cb63991f05fda1649c3fba63713e4
SFJT-97 <santiagojuarez.dev@gmail.com>
2024-03-16 11:38:55 -0400

    Array!

---
+++ Array/array.rb
@@ -0,0 +1,19 @@
+words1 = []
+
+words1.push("Hello", "I'm", "getting")
+
+words2 = ["World!", "just", "started."]
+
+hello = words1[0].to_str()
+im = words1[1].to_str()
+getting = words1[2].to_str()
+
+world = words2[0].to_str()
+just = words2[1].to_str()
+started = words2[2].to_str()
+
+space = " "
+
+puts hello + space + world + space + im + space + just + space + getting + space + started
+
+# Might not be the best way to do it, but it works. So it's good for now... First time Ruby, can't you tell? 
